7912b65497ea04e8acdaa2bda7da937800eb883a
jfietkau <julian@julian-fietkau.de>
2020-08-01 23:01:01 +0200

    Add support for differential build

--- makesite.py
+++ makesite.py
@@ -120,11 +120,54 @@ def render(template, **params):
                   template)
 
 
-def make_pages(src, dst, template, **params):
+def add_to_build(source, target, params):
+    link_if_bigger_than = 4 * 1024 * 1024
+    build_path = os.path.join(params['data_root'], 'build')
+    if target.startswith('/'):
+        target = target[1:]
+    target = os.path.join(params['site_dir'], target)
+    if not os.path.isfile(os.path.join(build_path, target)):
+        target_dir = os.path.dirname(os.path.join(build_path, target))
+        if not os.path.isdir(target_dir):
+            os.makedirs(target_dir)
+        # check if source is a path or direct file contents
+        if not os.path.isfile(source):
+            log('Adding {} from inline data ...'.format(target))
+            fwrite(os.path.join(build_path, target), source)
+        else:
+            log('Adding {} from {} ...'.format(target, source))
+            if os.path.getsize(source) > link_if_bigger_than:
+                os.symlink(source, os.path.join(build_path, target))
+            else:
+                shutil.copy2(source, os.path.join(build_path, target))
+    else:
+        target_stat = os.stat(os.path.join(build_path, target))
+        if not os.path.isfile(source):
+            target_content = fread(os.path.join(build_path, target))
+            if source != target_content:
+                log('Adding {} from inline data ...'.format(target))
+                fwrite(os.path.join(build_path, target), source)
+            else:
+                # log('Skipping {} - existing file is identical'.format(target))
+                pass
+        else:
+            source_stat = os.stat(source)
+            if source_stat.st_mtime != target_stat.st_mtime or source_stat.st_size != target_stat.st_size:
+                log('Adding {} from {} ...'.format(target, source))
+                if os.path.getsize(source) > link_if_bigger_than:
+                    os.symlink(source, os.path.join(build_path, target))
+                else:
+                    shutil.copy2(source, os.path.join(build_path, target))
+            else:
+                # log('Skipping {} - existing file is identical'.format(target))
+                pass
+
+
+def make_pages(page_list, destination, template, **params):
     """Generate pages from page content."""
     items = []
 
-    for src_path in glob.glob(src):
+    for src_path in page_list:
         if os.path.basename(src_path)[0].isdigit():
             continue
 
@@ -134,21 +177,18 @@ def make_pages(src, dst, template, **params):
 
         items.append(content)
 
-        dst_path = render(dst, **page_params)
+        dst_path = render(destination, **page_params)
         output = template.render(**page_params)
 
-        log('Rendering {} => {} ...', src_path, dst_path)
-        fwrite(os.path.join(params['target_root'], dst_path), output)
+        #log('Rendering {} ...', dst_path)
+        add_to_build(output, dst_path, params)
 
     return sorted(items, key=lambda x: x['date'], reverse=True)
-        
+
 
 def prepare_pub_files(pubs, params, template_env):
     source_dir = os.path.join(params['data_root'], 'content', 'science')
     cache_dir = os.path.join(params['data_root'], 'cache')
-    assets_dir = os.path.join(params['target_root'], 'assets')
-    if not os.path.isdir(assets_dir):
-        os.makedirs(assets_dir)
     for pub in pubs:
         pub_files = glob.glob(os.path.join(source_dir, str(pub['id'])+'.*'))
         for pub_file in pub_files:
@@ -156,48 +196,40 @@ def prepare_pub_files(pubs, params, template_env):
             if extension == '.html':
                pub['content_html'] = fread(pub_file)
                continue
-            target_path = os.path.join(params['target_root'], pub['url_id'] + extension)
-            shutil.copy2(pub_file, target_path)
+            add_to_build(pub_file, pub['url_id'] + extension, params)
             pub['has_download_'+extension[1:]] = True
             if extension == '.pdf':
                 thumbnail_path = os.path.join(cache_dir, pub['url_id'] + '_thumbnail.png')
                 if not os.path.isfile(thumbnail_path):
-                    subprocess.run(['convert', '-density', '600', target_path+'[0]',
+                    subprocess.run(['convert', '-density', '600', pub_file+'[0]',
                                     '-alpha', 'remove', '-resize', '600', thumbnail_path])
-                thumbnail_final_path = os.path.join(params['target_root'], 'assets', pub['url_id'] + '_thumbnail.png')
-                shutil.copy2(thumbnail_path, thumbnail_final_path)
+                add_to_build(thumbnail_path, os.path.join('assets', pub['url_id'] + '_thumbnail.png'), params)
                 pub['has_thumbnail'] = True
                 if not os.path.isfile(os.path.join(cache_dir, pub['url_id'] + '_page1.svg')):
                     svg_path = os.path.join(cache_dir, pub['url_id'] + '_page%d.svg')
-                    subprocess.run(['pdf2svg', target_path, svg_path, 'all'])
+                    subprocess.run(['pdf2svg', pub_file, svg_path, 'all'])
                 svg_pages = glob.glob(os.path.join(cache_dir, pub['url_id'] + '_page*.svg'))
                 for svg in svg_pages:
-                    svg_final_path = os.path.join(params['target_root'], 'assets', os.path.basename(svg))
-                    shutil.copy2(svg, svg_final_path)
+                    add_to_build(svg, os.path.join('assets', os.path.basename(svg)), params)
                 if len(svg_pages) > 0:
                     pub['content_svg'] = len(svg_pages)
         pub_template = template_env.get_template('science/publication-page.html')
         params['title'] = pub['title']
         output = pub_template.render(publication=pub, css='publication.css', **params)
-        fwrite(os.path.join(params['target_root'], pub['url_id']+'.html'), output)
+        add_to_build(output, pub['url_id']+'.html', params)
 
 
 def compile_site(site, params):
 
-    # Create a new output directory from scratch.
-    if os.path.isdir(params['target_root']):
-        try:
-            shutil.rmtree(params['target_root'])
-        except PermissionError:
-            pass
-
-    if not os.path.isdir(params['target_root']):
-        os.makedirs(params['target_root'])
-
     for static_source in ['all', site['name'].lower()]:
         static_path = os.path.join(params['data_root'], 'static', static_source)
         if os.path.isdir(static_path):
-            distutils.dir_util.copy_tree(static_path, params['target_root'])
+            everything = glob.glob(os.path.join(static_path, '**'), recursive=True)
+            for item in everything:
+                if os.path.isdir(item):
+                    continue
+                target_path = item[len(static_path):]
+                add_to_build(item, target_path, params)
 
     # Load templates.
     templates_path = os.path.join(params['data_root'], 'templates')
@@ -207,13 +239,15 @@ def compile_site(site, params):
     )
 
     # Create site pages.
-    content_path = os.path.join(params['data_root'], 'content')
+    content_path = os.path.join(params['data_root'], 'content', 'all')
     site_content_path = os.path.join(params['data_root'], 'content', site['name'].lower())
 
     page_template = template_env.get_template('page.html')
-    make_pages(os.path.join(content_path, '*.html'), '{{ slug }}.html',
-               page_template, **params)
-    make_pages(os.path.join(site_content_path, '*.html'), '{{ slug }}.html',
+    page_list = glob.glob(os.path.join(site_content_path, '*.html'))
+    for candidate in glob.glob(os.path.join(content_path, '*.html')):
+        if candidate.replace(content_path, site_content_path) not in page_list:
+            page_list.append(candidate)
+    make_pages(page_list, '{{ slug }}.html',
                page_template, **params)
 
     if site['name'] == 'Science':
@@ -229,10 +263,10 @@ def compile_site(site, params):
         pubs_template = template_env.get_template('science/publications.html')
         params['title'] = 'Publications'
         output = pubs_template.render(publications=pubs, **params)
-        fwrite(os.path.join(params['target_root'], 'publications.html'), output)
+        add_to_build(output, 'publications.html', params)
         feed_template = template_env.get_template('science/publications.xml')
         feed_output = feed_template.render(pubs=pubs, **params)
-        fwrite(os.path.join(params['target_root'], 'publications.xml'), feed_output)
+        add_to_build(feed_output, 'publications.xml', params)
 
         with open(os.path.join(params['data_root'], 'content', 'science', 'student_theses.json')) as fp:
             student_theses = json.load(fp)
@@ -241,27 +275,22 @@ def compile_site(site, params):
         student_theses.reverse()
         source_dir = os.path.join(params['data_root'], 'content', 'science')
         cache_dir = os.path.join(params['data_root'], 'cache')
-        assets_dir = os.path.join(params['target_root'], 'assets')
-        if not os.path.isdir(assets_dir):
-            os.makedirs(assets_dir)
         for thesis in student_theses:
             pdf_path = os.path.join(source_dir, str(thesis['url_id']) + '.pdf')
             if not os.path.isfile(pdf_path):
                 continue
-            target_path = os.path.join(params['target_root'], thesis['url_id'] + '.pdf')
             if thesis['enable_download']:
-                shutil.copy2(pdf_path, target_path)
+                add_to_build(pdf_path, thesis['url_id'] + '.pdf', params)
             thumbnail_path = os.path.join(cache_dir, thesis['url_id'] + '_thumbnail.png')
             if not os.path.isfile(thumbnail_path):
                 subprocess.run(['convert', '-density', '600', pdf_path+'[0]',
                                 '-alpha', 'remove', '-resize', '400', thumbnail_path])
-            thumbnail_final_path = os.path.join(params['target_root'], 'assets', thesis['url_id'] + '_thumbnail.png')
-            shutil.copy2(thumbnail_path, thumbnail_final_path)
+            add_to_build(thumbnail_path, os.path.join('assets', thesis['url_id'] + '_thumbnail.png'), params)
             thesis['has_thumbnail'] = True
         teaching_template = template_env.get_template('science/teaching.html')
         params['title'] = 'Teaching'
         output = teaching_template.render(student_theses=student_theses, **params)
-        fwrite(os.path.join(params['target_root'], 'teaching.html'), output)
+        add_to_build(output, 'teaching.html', params)
 
     if site['name'] == 'Software':
         with open(os.path.join(params['data_root'], 'content', 'software', 'projects.json')) as fp:
@@ -270,7 +299,7 @@ def compile_site(site, params):
         projects.sort(key=lambda p: p['title'].lower())
         template = template_env.get_template('software/index.html')
         output = template.render(projects=projects, **params)
-        fwrite(os.path.join(params['target_root'], 'index.html'), output)
+        add_to_build(output, 'index.html', params)
 
         template = template_env.get_template('software/projects.html')
         for category in ['major', 'minor']:
@@ -279,13 +308,13 @@ def compile_site(site, params):
             elif category == 'minor':
                 params['title'] = 'Smaller Tools'
             output = template.render(projects=projects, project_category=category, **params)
-            fwrite(os.path.join(params['target_root'], params['title'].lower().replace(' ', '_') + '.html'), output)
+            add_to_build(output, params['title'].lower().replace(' ', '_') + '.html', params)
 
         for proj in projects:
             template = template_env.get_template('software/project.html')
             params['title'] = proj['title']
             output = template.render(proj=proj, **params)
-            fwrite(os.path.join(params['target_root'], proj['url_id'] + '.html'), output)
+            add_to_build(output, proj['url_id'] + '.html', params)
 
     if site['name'] == 'Media':
         with open(os.path.join(params['data_root'], 'content', 'media', 'games.json')) as fp:
@@ -296,13 +325,13 @@ def compile_site(site, params):
         template = template_env.get_template('media/games.html')
         params['title'] = 'Games'
         output = template.render(projects=projects, **params)
-        fwrite(os.path.join(params['target_root'], 'games.html'), output)
+        add_to_build(output, 'games.html', params)
 
         for proj in projects:
             template = template_env.get_template('media/game.html')
             params['title'] = proj['title']
             output = template.render(proj=proj, **params)
-            fwrite(os.path.join(params['target_root'], proj['url_id'] + '.html'), output)
+            add_to_build(output, proj['url_id'] + '.html', params)
 
         with open(os.path.join(params['data_root'], 'content', 'media', 'videos.json')) as fp:
             videos = json.load(fp)
@@ -311,13 +340,13 @@ def compile_site(site, params):
         template = template_env.get_template('media/videos.html')
         params['title'] = 'Videos: Working with LaTeX'
         output = template.render(videos=videos, **params)
-        fwrite(os.path.join(params['target_root'], 'videos.html'), output)
+        add_to_build(output, 'videos.html', params)
 
         for video in videos:
             template = template_env.get_template('media/video.html')
             params['title'] = video['title']
             output = template.render(video=video, **params)
-            fwrite(os.path.join(params['target_root'], video['url_id'] + '.html'), output)
+            add_to_build(output, video['url_id'] + '.html', params)
 
         with open(os.path.join(params['data_root'], 'content', 'media', 'misc.json')) as fp:
             projects = json.load(fp)
@@ -326,19 +355,19 @@ def compile_site(site, params):
         template = template_env.get_template('media/miscs.html')
         params['title'] = 'Miscellaneous'
         output = template.render(projects=projects, **params)
-        fwrite(os.path.join(params['target_root'], 'misc.html'), output)
+        add_to_build(output, 'misc.html', params)
 
         for misc in projects:
             template = template_env.get_template('media/misc.html')
             params['title'] = misc['title']
             output = template.render(proj=misc, **params)
-            fwrite(os.path.join(params['target_root'], misc['url_id'] + '.html'), output)
+            add_to_build(output, misc['url_id'] + '.html', params)
 
     additional_templates = ['main.css']
     for additional_template in additional_templates:
         template = template_env.get_template(additional_template)
-        result = template.render(**params)
-        fwrite(os.path.join(params['target_root'], additional_template), result)
+        output = template.render(**params)
+        add_to_build(output, additional_template, params)
 
 
 def main():
@@ -349,13 +378,17 @@ def main():
 
     for site in params['sites']:
         site_params = copy.deepcopy(params)
-        site_params['target_root'] = os.path.join(site_params['target_root'], site['name'].lower())
+        del site_params['target_root']
+        site_params['site_dir'] = site['name'].lower()
         site_params['title'] = site['name']
         site_params['current_site'] = site['name']
         site_params['hostname'] = site['hostname']
         site_params['accent_color'] = site['accent_color']
         compile_site(site, site_params)
 
+    build_path = os.path.join(params['data_root'], 'build')
+    cmd = ['rsync', '--recursive', '--copy-links', '--safe-links', '--times', '--delete', build_path + '/', params['target_root'] + '/']
+    subprocess.run(cmd)
 
 # Test parameter to be set temporarily by unit tests.
 _test = None
